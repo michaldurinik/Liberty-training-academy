b1a93234d3dbbf08089acd35041b13b692349f5e
Michal Durinik <misko.durinik@gmail.com>
2019-07-09 10:18:46 +0100

    update flight-details

--- TypeScriptAndAngularProjects/angular/exercises/flights/src/app/model/flight-details.ts
+++ TypeScriptAndAngularProjects/angular/exercises/flights/src/app/model/flight-details.ts
@@ -1,7 +1,17 @@
-export interface FlightDetails {
+export class FlightDetails {
   number: number;
   origin: string;
   destination: string;
-  departure: string;
-  arrival: string;
+  departure: Date;
+  arrival: Date;
+
+  constructor(obj: any) {
+    this.number = parseInt(obj.number, 10);
+    this.origin = obj.origin;
+    this.departure = new Date(obj.departure);
+    this.destination = obj.destination;
+    this.arrival = new Date(obj.arrival);
+  }
 }
+
+
