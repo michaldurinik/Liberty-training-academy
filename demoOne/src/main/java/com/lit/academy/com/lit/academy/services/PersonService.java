f4d1bcb68bf0ad5a3c5d83b119ab6aa0f057fabd
MICHAL DURINIK <Michal.Durinik@liberty-it.co.uk>
2019-07-24 12:00:42 +0100

    update

--- demoOne/src/main/java/com/lit/academy/com/lit/academy/services/PersonService.java
+++ demoOne/src/main/java/com/lit/academy/com/lit/academy/services/PersonService.java
@@ -1,6 +1,7 @@
 package com.lit.academy.com.lit.academy.services;
 
 import com.lit.academy.model.Person;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,14 @@ public class PersonService {
     @Resource(name="sampleData")
     private List<Person> people;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Person> getAllPeps() {
-        return people;
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/{name}"
+    )
+public Person byName(@PathVariable String name) {
+        return people.stream()
+                .filter(p -> p.getName().equals(name))
+                .findFirst()
+                .orElse(new Person("Dummy", 0));
     }
 }
