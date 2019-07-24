b3bdb895db1f7f9f031e3a88e3567f07a975d0c3
MICHAL DURINIK <Michal.Durinik@liberty-it.co.uk>
2019-07-24 14:38:35 +0100

    initial broodwar spring-boot files

---
+++ broodwar/src/main/java/com/example/starcraft/broodwar/BroodwarApplication.java
@@ -0,0 +1,13 @@
+package com.example.starcraft.broodwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroodwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroodwarApplication.class, args);
+	}
+
+}
